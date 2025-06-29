e3afb30e1c634754b53ba8ced23f982a55e93206
reantus <339698418@qq.com>
2025-06-29 18:40:11 +0800

    props children

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Person from "./components/person.jsx"
-import Product from "./components/product.jsx"
+import Card from "./components/card.jsx"
 
 const App = () => {
   return (
     <>
-     <Person name='Huxn' age={22}/>
-     <Product name='Ipone' price={30000}/>
+      <h1>My Card</h1>
+      <Card>
+        <h2>Card Title</h2>
+        <p>This is some content for card 1</p>
+      </Card>
+      <Card>
+        <h2>Card Title</h2>
+        <p>This is some content for card 2</p>
+      </Card>
     </>
   )
 }
