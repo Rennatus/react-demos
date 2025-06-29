b6623b9311159efcf8a2b507fcc0e935e93f43a8
reantus <339698418@qq.com>
2025-06-29 20:31:06 +0800

    events

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import StyledCard from "./components/style_card.jsx";
-import ProfileCard from "./components/profile_card.jsx";
-import IconConmponet from "./components/icon_conmponet.jsx";
+import EventDemo from "./components/event_demo";
 
 const App = () => {
   return (
     <>
-      <StyledCard />
-      <ProfileCard />
-      <IconConmponet />
+      <EventDemo />
     </>
   );
 };
