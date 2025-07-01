ec73325aeda1659051ae67ec70f37b6c0a70e6ce
reantus <339698418@qq.com>
2025-07-01 15:28:53 +0800

    projece:todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
     <div>
-      <Counter />
+      <Todo />
     </div>
   );
 };
