8377522bc91248816ecabc9a5cc3f3781eb195c9
reantus <339698418@qq.com>
2025-07-01 16:29:15 +0800

    project:calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Meals from "./components/Meals";
+import Calculator from "./components/Calculator";
 const App = () => {
   return (
     <div>
-      <Meals />
+      <Calculator />
     </div>
   );
 };
